c23e3dd154c92cfc0461184b1a61ab42cd13f77f
Devipurwita24 <61130340+Devipurwita24@users.noreply.github.com>
2022-12-15 17:01:18 +0700

    add link scrollto

--- resources/views/main/navbar.blade.php
+++ resources/views/main/navbar.blade.php
@@ -1,34 +1,3 @@
-{{-- <nav class="navbar navbar-expand-lg navbar-light navbar-header fixed-top">
-    <div class="container-fluid">
-        <a href="#" class="navbar-brand logo ml-1"><img src="assets/images/logo.png" alt="logo" width="100%"></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item active">
-                    <a class="nav-link scrollto" href="#">Home</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link scrollto" href="#about-event">About Us</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link scrollto" href="#register-login">Industries</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link scrollto" href="#register-login">Capabilities</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link scrollto" href="#register-login">Equipment</a>
-                </li>
-                <li class="nav-item active">
-                    <a class="nav-link scrollto" href="#register-login">Talk to Us</a>
-                </li>
-            </ul>
-        </div>
-    </div>
-</nav> --}}
 <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top shadow-lg">
     <div class="container-fluid">
         <a href="/" class="navbar-brand logo ml-1 mx-4"><img src="assets/images/img_indosol_logo.png" alt="logo" width="100%"></a>
@@ -48,10 +17,10 @@
                     <a class="nav-link scrollto" style="color: #4D4D4F;" href="#who_we_are">Industries</a>
                 </li>
                 <li class="nav-item mx-3">
-                    <a class="nav-link scrollto" style="color: #4D4D4F;" href="#capabilities">Capabilities</a>
+                    <a class="nav-link scrollto" style="color: #4D4D4F;" href="#what_we_do">Capabilities</a>
                 </li>
                 <li class="nav-item mx-3">
-                    <a class="nav-link scrollto" style="color: #4D4D4F;" href="#equipment">Equipment</a>
+                    <a class="nav-link scrollto" style="color: #4D4D4F;" href="#client">Equipment</a>
                 </li>
                 <li class="nav-item mx-3">
                     <a class="nav-link scrollto" style="color: #4D4D4F;" href="#talk_to_us">Talk to Us</a>
